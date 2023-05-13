093f3d85ae57e6c6ade256e435ed3dbae0682731
izabelaoska <izabela.oska@gmail.com>
2023-05-13 19:51:19 +0200

    fix: rounded corners in MainCard

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const MainCard = () => {
-  return <div className="h-full p-16 shadow-xl rounded-2xl bg-base-200"></div>
+  return <div className="h-full p-16 shadow-xl rounded-3xl bg-base-200"></div>
 }
