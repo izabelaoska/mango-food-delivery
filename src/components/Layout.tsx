05c52a6a548fdc1f583aace61570bc003f4a3080
izabelaoska <izabela.oska@gmail.com>
2023-05-31 20:28:38 +0100

    fix: improving the MainCard

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { MainCard } from './MainCard'
 export const Layout = () => {
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-[#FFE3BA] to-[#F5D7AC]">
-      <div className="h-full px-16 py-12">
+      <div className="h-full px-32 py-12">
         <MainCard />
       </div>
     </div>
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Navbar } from './Navbar'
 
 export const MainCard = () => {
-  return <div className="h-full p-16 shadow-xl rounded-3xl bg-base-200"></div>
+  return (
+    <div className="flex w-full h-full">
+      <div className="flex flex-row w-full shadow-xl rounded-3xl bg-base-200">
+        <Navbar />
+        <div className="flex-grow" />
+      </div>
+    </div>
+  )
 }
