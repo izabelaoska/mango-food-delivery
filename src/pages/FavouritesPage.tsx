c632eeb5cce6fe64996d8bcaa4374e905f980018
izabelaoska <izabela.oska@gmail.com>
2023-06-29 19:20:58 +0100

    fix: width

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,6 +1,6 @@
 export const FavouritesPage = () => {
   return (
-    <div className="flex-grow bg-[#F9F9FB] rounded-tr-3xl rounded-br-3xl">
+    <div className="flex-grow w-full bg-[#F9F9FB] rounded-tr-3xl rounded-br-3xl">
       Favourites Page
     </div>
   )
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,6 +1,6 @@
 export const SettingsPage = () => {
   return (
-    <div className="bg-[#F9F9FB] flex-grow rounded-tr-3xl rounded-br-3xl">
+    <div className="bg-[#F9F9FB] flex-grow w-full rounded-tr-3xl rounded-br-3xl">
       Settings Page
     </div>
   )
